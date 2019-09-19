e386e8ab315cdcb38255e55f0fcb2d6e779ce355
schaugf <schau.geoffrey@gmail.com>
2019-09-19 08:49:18 -0700

    img saver

--- src/xae_torch.py
+++ src/xae_torch.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 import os
 import argparse
-from random import shuffle
 from PIL import Image
 import numpy as np
 import pandas as pd
@@ -13,21 +12,30 @@ from torch.autograd import Variable
 from torch.nn import functional as F
 from torch.nn import init
 from torchvision import datasets, transforms
-#from torchsummary import summary
 
 
 parser = argparse.ArgumentParser(description='XAE Model')
 
-parser.add_argument('--batch_size', type=int, default=12, metavar='N',
-                    help='input batch size for training (default: 12)')
-parser.add_argument('--added_noise', type=float, default=0.2, metavar='N',
+parser.add_argument('--batch_size', type=int, default=8, metavar='N',
+                    help='input batch size for training (default: 10)')
+parser.add_argument('--lr', type=float, default=1e-3, metavar='N',
+                    help='optimizer learning rate (default: 1e-3)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 5)')
+parser.add_argument('--save_dir', type=str, default='../results/test', 
+                    metavar='N', help='save directory')
+parser.add_argument('--dataset', type=str, default='test', metavar='N',
+                    help='dataset to load (mnist or test)')
+
+parser.add_argument('--latent_dim', type=int, default=16, metavar='N',
+                    help='how big is z')
+parser.add_argument('--inter_dim', type=int, default=128, metavar='N',
+                    help='how big is linear around z')
+
+parser.add_argument('--added_noise', type=float, default=0.5, metavar='N',
                     help='fraction of noise added to input')
 parser.add_argument('--do_gate_layer', type=int, default=1, metavar='N',
                     help='append weighted gate layer to input?')
-parser.add_argument('--do_gate_recon', type=int, default=1, metavar='N',
-                    help='gate reconstruction?')
-parser.add_argument('--gate_recon_lambda', type=int, default=250, metavar='N',
-                    help='weight gate reconstruction term')
 
 parser.add_argument('--A_med_lambda', type=float, default=10.0, metavar='N',
                     help='A mes loss coefficient')
@@ -44,28 +52,15 @@ parser.add_argument('--A_vae_lambda', type=float, default=1.0, metavar='N',
 parser.add_argument('--B_vae_lambda', type=float, default=1.0, metavar='N',
                     help='B vae loss coefficient')
 
-
-parser.add_argument('--n_epoch_set_binary', type=int, default=20, metavar='N',
+parser.add_argument('--n_epoch_set_binary', type=int, default=5, metavar='N',
                     help='how often to set binary gate layer')
+parser.add_argument('--quantile_cutoff', type=float, default=0.8, metavar='N',
+                    help='quantile of noise weights to set to zero')
 
-parser.add_argument('--lr', type=float, default=1e-3, metavar='N',
-                    help='optimizer learning rate (default: 1e-3)')
-parser.add_argument('--epochs', type=int, default=5, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--latent_dim', type=int, default=20, metavar='N',
-                    help='how big is z')
-parser.add_argument('--inter_dim', type=int, default=128, metavar='N',
-                    help='how big is linear around z')
-parser.add_argument('--dataset', type=str, default='test', metavar='N',
-                    help='dataset to load (mnist or test)')
-parser.add_argument('--save_dir', type=str, default='../results/test', 
-                    metavar='N', help='save directory')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging status')
+parser.add_argument('--seed', type=int, default=1)
+
 args = parser.parse_args()
 
                    
@@ -76,6 +71,8 @@ if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+
+# TODO: delete whatever is inside
 os.makedirs(args.save_dir, exist_ok = True)
 
 
@@ -143,7 +140,19 @@ print('corrupted omic test shape', test_ome.shape)
 A_shape = train_img.shape[1:]
 B_shape = train_ome.shape[1:]
 
+# build data loaders
+train_t = torch.utils.data.TensorDataset(torch.tensor(train_img).float(), 
+                                         torch.tensor(train_ome).float())
 
+train_loader = torch.utils.data.DataLoader(train_t, 
+                                            batch_size = args.batch_size, 
+                                            shuffle = True)
+
+eval_loader = torch.utils.data.DataLoader(train_t, 
+                                          batch_size = args.batch_size, 
+                                          shuffle = False)
+
+# custom layer definitions
 class Flatten(nn.Module):
         def __init__(self):
             super(Flatten, self).__init__()
@@ -167,15 +176,15 @@ class GateLayer(nn.Module):
         self.in_shape = in_shape
         self.weight = nn.Parameter(torch.Tensor(1, in_shape))
         init.kaiming_uniform_(self.weight)
-      #  self.binary_layer = torch.tensor(np.ones(in_shape))
-      #  self.binary_layer.requires_grad = False
+        
+        self.binary_layer = nn.Parameter(torch.FloatTensor(np.ones(in_shape)))
+        self.binary_layer.requires_grad = False
 
     def forward(self, x):
-        return x * self.weight #* self.binary_layer
+        return x * self.weight * self.binary_layer
 
 
-class XAE(nn.Module):
-    
+class XAE(nn.Module):    
     def __init__(self, 
                  A_type = 'img', 
                  B_type = 'ome',
@@ -217,7 +226,6 @@ class XAE(nn.Module):
 
     def make_image_encoder(self, in_shape):
         
-        # need: number of input channels, image size
         img_encoder = nn.Sequential(
                 nn.Conv2d(1, 64, kernel_size=3, stride=2, padding=1),
                 nn.ReLU(),
@@ -295,15 +303,14 @@ class XAE(nn.Module):
         
 
     def reparameterize(self, mu, logvar):
-        
+        # vae reparameterization trick
         std = torch.exp(0.5*logvar)
         eps = torch.randn_like(std)
         return mu + eps*std
 
 
     def forward(self, A_in, B_in):
-        
-        # autoencoders
+        # xae autoencoders
         A_inter = self.A_encoder(A_in)
         A_mu, A_logvar = self.shared_fc1(A_inter), self.shared_fc2(A_inter)
         A_z = self.reparameterize(A_mu, A_logvar)
@@ -379,10 +386,10 @@ def cycle_loss(recon_x, x):
 
 def vce_loss(recon_x, x, mu_1, logvar_1, mu_2, logvar_2):
     # variational cyclic encoder
-    # cycle consistency loss with KL divergence
     CC = F.binary_cross_entropy(recon_x.view(-1, np.prod(recon_x.shape[1:])), 
                                 x.view(-1, np.prod(x.shape[1:])),
                                 reduction = 'sum')
+    
     KLD_1 = -0.5 * torch.sum(1 + logvar_1 - mu_1.pow(2) - logvar_1.exp())
     KLD_2 = -0.5 * torch.sum(1 + logvar_2 - mu_2.pow(2) - logvar_2.exp())
     
@@ -400,9 +407,12 @@ def train(epoch, is_final):
     # TODO: define this in main loop
     loss_keys = ['A_vce_loss',
                  'B_vce_loss',
+                 'A_vae_loss',
+                 'B_vae_loss',
                  'A_med_loss',
                  'B_med_loss',
                  'total_loss']
+    
     all_losses = []
 
     for batch_idx, (A_data, B_data) in enumerate(train_loader):
@@ -416,10 +426,10 @@ def train(epoch, is_final):
             
         optimizer.zero_grad()
         
+        # data through model
         return_dict = model(A_data, B_data)
         
         # compute losses
-        
         A_vce_loss = vce_loss(recon_x = return_dict['A2B2A_rec'], 
                               x = A_data, 
                               mu_1 = return_dict['A_mu'], 
@@ -457,22 +467,22 @@ def train(epoch, is_final):
                    args.A_med_lambda * A_med_loss + \
                    args.B_med_lambda * B_med_loss
         
-          
         xae_loss.backward()
         optimizer.step()
         
         # TODO: if do_vae_loss, remove from storage
         loss_vals = [A_vce_loss.item(),
                      B_vce_loss.item(),
+                     A_vae_loss.item(),
+                     B_vae_loss.item(),
                      A_med_loss.item(),
                      B_med_loss.item()]
         
         all_losses.append(dict(zip(loss_keys, loss_vals)))
             
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, xae_loss / len(train_loader.dataset)))
+    # at end of epoch
+    print('Epoch:', epoch, 'XAE loss:', round(xae_loss.item(), 0))
 
-    # append losses for each epoch to dataframe
     mean_loss = pd.DataFrame(pd.DataFrame(all_losses).mean()).T
     mean_loss['epoch'] = epoch
     
@@ -482,6 +492,13 @@ def train(epoch, is_final):
                          index = False)
     
     # save batch of reconstructions
+    if (epoch % args.n_epoch_set_binary) == 0:
+        set_binary_layer(epoch)
+
+    save_batch_reconstructions(return_dict, A_data, epoch)
+    
+    
+def save_batch_reconstructions(return_dict, A_data, epoch):
     nrec = 8
     gt_imgs = A_data[:nrec,0,:].detach().cpu().numpy()
     gt_stack = np.concatenate(list(gt_imgs))
@@ -517,22 +534,53 @@ def train(epoch, is_final):
     to_save = Image.fromarray((255*imgs_to_save).astype(np.uint8))
     to_save.save(os.path.join(args.save_dir, 'image_reconstructions.png'))
     
-    # TODO: set binary layer
-#    if (epoch % args.n_epoch_set_binary) == 0:
-#        gate_weights = model.B_encoder[0].weight.detach().cpu()
-#        gwpd = pd.DataFrame({'gate_weights':gate_weights.numpy()[0]})
-#        is_noise = np.ones(gwpd.shape[0])
-#        is_noise[:B_init_dim[0]] = 0
-#        gwpd['is_noise'] = is_noise
-#        noise_weight = gwpd['gate_weights'][gwpd['is_noise']==1]
-#        # compute .95 quantile for noise weights
-#        noise_quantile_cutoff = (noise_weight**2).quantile(0.95)
-#        # set binary layer
-#        binary_layer_set = (gwpd['gate_weights']**2) > noise_quantile_cutoff
-#        binary_layer_set = nn.Parameter(torch.tensor(binary_layer_set.astype(float)))
-#        model.B_encoder[0].weight = binary_layer_set
-        
 
+def set_binary_layer(epoch):       
+    gate_weights = model.B_encoder[0].weight.detach().cpu()
+    gwpd = pd.DataFrame({'gate_weights':gate_weights.numpy()[0]})
+    is_noise = np.ones(gwpd.shape[0])
+    is_noise[:B_init_dim[0]] = 0
+    gwpd['is_noise'] = is_noise
+    noise_weight = gwpd['gate_weights'][gwpd['is_noise']==1]
+    # compute quantile for noise weights
+    noise_quantile_cutoff = (noise_weight**2).quantile(args.quantile_cutoff)
+    # set binary layer
+    binary_layer_set = (gwpd['gate_weights']**2) > noise_quantile_cutoff
+    binary_layer_set = nn.Parameter(torch.tensor(binary_layer_set.astype(float)))
+    model.B_encoder[0].binary_layer = binary_layer_set
+    save_gate_weights(epoch)
+    
+    
+def save_gate_weights(epoch):
+    gate_weights = model.B_encoder[0].weight
+    gate_binary = model.B_encoder[0].binary_layer
+    
+    gw = pd.DataFrame({'gate_weights': gate_weights.detach().cpu()[0].numpy().T,
+                       'gate_binary' : gate_binary.detach().cpu().numpy().T})
+    
+    gw.to_csv(os.path.join(args.save_dir, 'gate_weights.csv'), index=False)
+    
+    # remove elements to make square matrix
+    nrm = np.mod(gw.shape[0], train_img.shape[2])
+    if nrm !=0 :
+        gw = gw.iloc[:-nrm,]
+    
+    # TODO: check if image exists in filesystem, load else create
+    
+    # TODO: load image, append as in reconstruction
+    g = np.array(gw['gate_weights']).reshape(-1, train_img.shape[2])
+    g = (255*((g**2)/(g**2).max())).astype(np.uint8)
+    Image.fromarray(g).save(os.path.join(args.save_dir, 'gate_2d.jpg'))
+    
+    # binary weights
+    b = np.array(gw['gate_binary']).reshape(-1, train_img.shape[2])
+    b = (b*255).astype(np.uint8)
+    Image.fromarray(b).save(os.path.join(args.save_dir, 'gate_binary_2d.jpg'))
+
+    #Image.fromarray(g).show()
+    #Image.fromarray(b).show()
+    
+    
 def encode_all():
     A_imgs = []
     A_predicted_imgs = []
@@ -591,6 +639,7 @@ def encode_all():
     B_data_full = torch.tensor(np.array(pd.read_csv(os.path.join(args.save_dir, 
                                                                  'B_data.csv'),
                                          header=None)))
+    
     A2B_pred_full = torch.tensor(np.array(pd.read_csv(os.path.join(args.save_dir, 
                                                                    'A2B_prediction.csv'),
                                          header=None)))
@@ -610,49 +659,13 @@ def encode_all():
     
     pd.DataFrame(B2A_xents).to_csv(os.path.join(args.save_dir, 'B2A_xent.csv'),
                  header=None, index=False)
-    
-    # save gate weights
-    if args.do_gate_layer:
-        gate_weights = model.B_encoder[0].weight
-        pd.DataFrame(gate_weights.detach().cpu().numpy()).T.to_csv(
-                os.path.join(args.save_dir, 
-                             'gate_weights.csv'))
         
-        # save gate weight image
-        g = gate_weights.detach().cpu().numpy()[0]
-        nrm = np.mod(len(g), train_img.shape[2])
-        if nrm != 0:
-            g = g[:-nrm]
-        g = g.reshape(-1, train_img.shape[2])
-        g = g / g.max()
-        g = g ** 2
-        mx = (g*255).astype(np.uint8)
-        
-        Image.fromarray(mx).save(os.path.join(args.save_dir, 'gate_2d.jpg'))
-        #Image.fromarray(mx).show()
 
-
-train_t = torch.utils.data.TensorDataset(torch.tensor(train_img).float(), 
-                                         torch.tensor(train_ome).float())
-
-train_loader = torch.utils.data.DataLoader(train_t, 
-                                            batch_size = args.batch_size, 
-                                            shuffle = True)
-
-eval_loader = torch.utils.data.DataLoader(train_t, 
-                                          batch_size = args.batch_size, 
-                                          shuffle = False)
-
-if __name__ == "__main__":    
+if __name__ == "__main__":
     for epoch in range(1, args.epochs + 1):
         # TODO: shuffle tensor/array in better way
-        #shuffle(train_img)
-        #shuffle(train_ome)
-        
-        train(epoch, is_final = epoch == args.epochs)
-        
-        # TODO: program binary layer
         # TODO: build test function
+        train(epoch, is_final = epoch == args.epochs)
         
     print('encoding all')
     encode_all()
